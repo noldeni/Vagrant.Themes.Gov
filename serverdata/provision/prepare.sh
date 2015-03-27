40123154a35d75be2f1f792f8c04a4d52934d789
Ingo Nolden <dev@nolden.org>
2015-03-27 17:01:00 +0100

    [TASK] Simplified key adding (no download neccesary)

--- serverdata/provision/prepare.sh
+++ serverdata/provision/prepare.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 if [ ! -f /installed-aptkey-php54 ]; then
 	touch /installed-aptkey-php54
-	wget http://www.dotdeb.org/dotdeb.gpg
-    cat dotdeb.gpg | apt-key add -
+    wget -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -
 fi
