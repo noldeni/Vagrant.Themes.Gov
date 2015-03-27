930e8d3bf7337a2116e603cce4b64df60d0c28a5
Ingo Nolden <dev@nolden.org>
2015-03-27 17:28:23 +0100

    [TASK] Adjust paths

--- serverdata/updateTypo3Config.sh
+++ serverdata/updateTypo3Config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp /var/www/typo3conf/PackageStates.php      /vagrant/project/typo3conf/PackageStates.php
-cp /var/www/typo3conf/LocalConfiguration.php /vagrant/project/typo3conf/LocalConfiguration.php
+cp /var/www/typo3conf/PackageStates.php      /project/typo3conf/PackageStates.php
+cp /var/www/typo3conf/LocalConfiguration.php /project/typo3conf/LocalConfiguration.php
